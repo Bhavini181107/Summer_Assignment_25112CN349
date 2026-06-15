176afe60f9431b385153cec601b572818b19e273
Bhavini181107 <singhbhavini9@gmail.com>
2026-06-15 18:49:16 +0530

    Day_12 assignment is uploaded.

---
+++ Day_12/question_45.c
@@ -0,0 +1,33 @@
+//Write a function for pallindrome.
+#include<stdio.h>
+
+int palindrome(int n)
+{
+    int temp = n, rev = 0;
+
+    while(n > 0)
+    {
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+
+    if(temp == rev)
+        return 1;
+    else
+        return 0;
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(palindrome(n))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+
+    return 0;
+}
