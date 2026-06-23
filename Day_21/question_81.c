ba7e553cefb7b99f2dfc38faf4ddff4517847e66
Bhavini181107 <singhbhavini9@gmail.com>
2026-06-24 01:35:05 +0530

    Day_21 assignment is uploaded.

---
+++ Day_21/question_81.c
@@ -0,0 +1,20 @@
+//WAP to find string lenght without using strlen().
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[len] != '\0')
+        len++;
+
+    if(str[len-1] == '\n')
+        len--;
+
+    printf("Length = %d", len);
+
+    return 0;
+}
---
+++ Day_21/question_83.c
@@ -0,0 +1,26 @@
+//WAP to count vowels and consonants.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, vowels = 0, consonants = 0;
+
+    printf("Enter string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i=0; str[i]!='\0'; i++) {
+        if((str[i]>='A' && str[i]<='Z') ||
+           (str[i]>='a' && str[i]<='z')) {
+
+            if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||
+               str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    printf("Vowels = %d\nConsonants = %d", vowels, consonants);
+
+    return 0;
+}
