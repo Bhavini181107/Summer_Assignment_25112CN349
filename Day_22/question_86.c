97e062d0a23cf2ae68048017b643876d46e79ffa
Bhavini181107 <singhbhavini9@gmail.com>
2026-06-24 01:39:47 +0530

    Day_22 assignment is uploaded.

---
+++ Day_22/question_86.c
@@ -0,0 +1,19 @@
+//WAP  to count words in a sentence.
+#include <stdio.h>
+
+int main() {
+    char str[200];
+    int i, words = 1;
+
+    printf("Enter sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ' ' && str[i+1] != ' ')
+            words++;
+    }
+
+    printf("Number of words = %d", words);
+
+    return 0;
+}
